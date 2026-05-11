e655ae7d2035117657f3fa23a7b2901916b779d9
BlueHades74 <156944645+BlueHades74@users.noreply.github.com>
2026-05-10 22:23:28 -0600

    started movement

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using Unity.Netcode;
+
+public class PlayerMovement : NetworkBehaviour
+{
+
+    [SerializeField] private float moveSpeed = 5f;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        if (IsOwner)
+        {
+            GetComponent<Renderer>().material.color = Color.blue;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
